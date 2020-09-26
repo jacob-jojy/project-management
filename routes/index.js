61459c43fb1bed13f907a6cfa499b6b584103010
Rubz07 <70872509+Rubz07@users.noreply.github.com>
2020-09-26 12:01:39 -0700

    hashed password and enterd password checked

--- routes/index.js
+++ routes/index.js
@@ -10,24 +10,46 @@ const salt = 10;
 /*user array to save users data*/
 
 users = [];
-/*hashing the user entered password*/
 
 router.get("/", (req, res) => {
   res.render("index");
 });
-router.post("/", (req, res) => {
+/*hashing the user entered password*/
+
+/*router.post("/", (req, res) => {
   const hashedPassword = bcrypt.hash(req.body.password, salt, (err, hash) => {
     if (err) throw err;
     const user = { username: req.body.login, password: hash };
-    users.push(user);
     db.get()
       .collection("users")
       .insertOne(user, (err, data) => {
         if (err) throw err;
       });
   });
-});
+});*/
+
 /*compare the hashed password and user entered password*/
-router.post("/login", (req, res) => {});
+router.post("/", (req, res) => {
+  var username = req.body.login;
+  var password = req.body.password;
+  db.get()
+    .collection("users")
+    .find({ username: username })
+    .toArray((err, user) => {
+      if (err) console.log(err);
+      if (!user[0]) {
+        console.log("user not found");
+        res.status(404).send("user not found");
+      } else {
+        let bool = bcrypt.compareSync(password, user[0].password);
+        console.log(bool);
+        if (bool == false) {
+          res.status(400).send("OOPS!!!invalid password");
+        } else {
+          res.send("WELCOME " + username);
+        }
+      }
+    });
+});
 
 module.exports = router;
